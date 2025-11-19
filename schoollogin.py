a4b463813d4a7bd2256d10d21eac98b0d91e6c08
carsons11 <carsons11@nycstudents.net>
2025-11-18 20:42:53 -0500

    Update schoollogin.py

--- schoollogin.py
+++ schoollogin.py
@@ -1,19 +1,24 @@
-def validemail(email, password):
-    passwordcount = 0
-    passwordint = False
-    if "@" not in email:
-        return "Error: Invalid Email Format"
-    for i in password:
-        passwordcount+=1
-        if password.isdigit():
-            passwordint = True
-    if passwordint != True:
-        return "Error: Your password must contain at least one numerical digit."
-    if passwordcount < 8:
-        return "Error: Your password is too short. It must be at least 8 characters."
+# def validemail(email, password):
+#     passwordcount = 0
+#     passwordint = False
+#     if "@" not in email:
+#         return "Error: Invalid Email Format"
+#     for i in password:
+#         passwordcount+=1
+#         if password.isdigit():
+#             passwordint = True
+#     if passwordint != True:
+#         return "Error: Your password must contain at least one numerical digit."
+#     if passwordcount < 8:
+#         return "Error: Your password is too short. It must be at least 8 characters."
         
         
         
-        print ("Your account has successfully been created.")
-validemail()
-    
+#         print ("Your account has successfully been created.")
+# validemail()
+test = "abcdf1"
+def testing(test):
+    for i in test:
+        if test.isdigit():
+            print ("success")
+testing(test)
