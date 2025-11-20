6c4a03106b2f8be4b5ea12a281f22ced0952a74b
carsons11 <carsons11@nycstudents.net>
2025-11-20 09:01:41 -0500

    Update schoollogin.py

--- schoollogin.py
+++ schoollogin.py
@@ -1,29 +1,16 @@
 def validemail(email, password):
-    emailvalid = False
-    passwordlength = False
-    passwordint = False
-    passwordupper = False
     if type(email) != str:
-        print ("Error: Your email must be in a string datatype format.")
+        return ("Error: Your email must be in a string datatype format.")
     if type(password) != str: 
-        print ("Error: Your password must be in a string datatype format.")
+        return ("Error: Your password must be in a string datatype format.")
     if "@" not in email:
-        print ("Error: Invalid Email Format. Your email must contain an @ symbol.")
-    else: 
-        emailvalid = True
+        return ("Error: Invalid Email Format. Your email must contain an @ symbol.")
     if len(password) < 8:
-        print ("Error: Your password must be at least 8 digits.")
-    else:
-        passwordlength = True
-    for i in password:
-        if i.isdigit():
-            passwordint = True
-        if i.isupper():
-            passwordupper = True
-    if passwordint != True:
-        print ("Error: Your password must contain at least one numerical digit.")
-    if passwordupper != True:
-        print ("Error: Your password must contain at least one uppercase letter.")
-    if emailvalid == passwordlength == passwordint == passwordupper == True:
-        print (f"Your account has successfully been created. Here is your email: {email} and password: {password}.")
-validemail("testing@gmail.com", "Testing12")
+        return ("Error: Your password must be at least 8 digits.")
+    if not any (i.isdigit() for i in password):
+        return ("Error: Your password must contain at least one mumerical digit.")
+    if not any (i.isupper() for i in password):
+        return ("Error: Your password must contain at least one uppercase letter.")
+    else:   
+        return (f"Your account has successfully been created. Here is your email: {email} and password: {password}.")
+print (validemail("testing@gmail.com", "Testing2")) 
